d0b9bc72645ee47fe9d316175df6370f07dd2701
copper280z <rday689@gmail.com>
2023-12-28 22:58:06 -0500

    start to implementation of kalman c++ class

--- src/Kalman/Kalman.cpp
+++ src/Kalman/Kalman.cpp
@@ -1,1 +1,13 @@
 #include "Kalman.h"
+
+float* Kalman::update(float dt, float[n_outputs] z, float[n_inputs] u) {
+    xp_hat = xp_hat + (A*x_hat + B*u)*dt;
+    P_hat = F*P*(~F) + Q
+    S = H*P_hat*(~H) + R
+    K = P_hat*(~H)*Invert(S)
+    P = (I-K*H)*P_hat
+    y = z-(H*x_hat)
+    x_hat = xp_hat + K*y
+
+    return x_hat.storage;
+}
--- src/Kalman/Kalman.h
+++ src/Kalman/Kalman.h
@@ -1,7 +1,31 @@
 #pragma once
+#include "BasicLinearAlgebra.h"
 
+using namespace BLA;
+template <int n_inputs, int n_outputs, int n_states>
 class Kalman {
     public:
-    
+        Matrix<n_states> x_hat;
+
+        // System Matrix, defines time update behavior
+        Matrix<n_states, n_states> F;
+        // Input Matrix
+        Matrix<n_states, n_inputs> B;
+        // Measurement Matrix
+        Matrix<n_states, n_outputs> H;
+        // Process Noise
+        Matrix<n_states, n_states> Q;
+        // Sensor Noise, stdev^2
+        Matrix<n_outputs, n_outputs> R;
+        // Measurement Matrix
+        Matrix<n_outputs> y;
+
+        // performs the filter update at a timestep
+        float* update(float dt, float[n_outputs] z, float[n_inputs] u);
     private:
+        Matrix<n_states> xp_hat;
+        Matrix<n_states, n_states> I;
+        Matrix<n_states, n_states> P;
+        Matrix<n_states, n_states> P_hat;
+        Matrix<n_states, n_inputs> K;
 }
