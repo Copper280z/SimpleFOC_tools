c0c23de3e3b28c876ca8254e09a1675d980ea923
copper280z <rday689@gmail.com>
2024-01-02 08:33:37 -0500

    Added section for simple binary data transfer over serial with initial implementation. Protocol needs work, also needs host side python script to recieve and decode data.

---
+++ src/BinaryUART/BinaryUART.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Arduino.h"
+
+
+
+void send_array(Stream *ser, float* arr, int num);
+// void send_array(Stream ser, uint8_t* arr, int num);
+// void send_array(Stream ser, uint16_t* arr, int num);
+// void send_array(Stream ser, uint32_t* arr, int num);
---
+++ src/BinaryUART/Testing/binary_sender/src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+// #include "../../../BinaryUART.h"
+#include <SerialCommands.h>
+
+void send_array(Stream *ser, float* arr, int num) {
+    uint32_t num_bytes = sizeof(arr[0])*num;
+    ser->printf("sending %d bytes:", num_bytes);
+    for (int i=0; i<num; i++) {
+        ser->write(arr[i]);
+    }
+    
+}
+
+float array[4096];
+char serial_command_buffer_[32];
+SerialCommands serial_commands_(&Serial, serial_command_buffer_, sizeof(serial_command_buffer_), "\r\n", " ");
+
+void cmd_unrecognized(SerialCommands* sender, const char* cmd)
+{
+  sender->GetSerial()->print("Unrecognized command [");
+  sender->GetSerial()->print(cmd);
+  sender->GetSerial()->println("]");
+}
+
+void cmd_send_arr(SerialCommands* sender) {
+  send_array(&Serial, array, 4096);
+}
+
+SerialCommand cmd_send_("send", cmd_send_arr);
+
+void setup() {
+  Serial.begin(115200);
+  serial_commands_.SetDefaultHandler(cmd_unrecognized);
+  serial_commands_.AddCommand(&cmd_send_);
+
+  for (int i=0; i<4096; i++) {
+    array[i] = (float) i;
+  }
+
+}
+
+void loop() {
+  serial_commands_.ReadSerial();
+
+}
